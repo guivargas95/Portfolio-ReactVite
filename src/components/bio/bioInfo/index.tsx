92abfc189ac77e4554481759dc867a51f6a12c7d
Guilherme Vargas <guisouzavargas@outlook.com>
2022-07-26 13:01:19 -0300

    correção dos links da BIO

--- src/components/bio/bioInfo/index.tsx
+++ src/components/bio/bioInfo/index.tsx
@@ -18,16 +18,16 @@ export default function BioInfo() {
     }
 
     return (
-        <div className="items-center mt-2 flex flex-col md:content-around lg:mr-5">
+        <div className="items-center mt-2 flex flex-col md:content-around md:mt-0 lg:mr-5">
             <div className="p-3 font-padrao">
                 <button onClick={TextoPtBr} className="p-1 m-1 rounded-sm bg-paleta-cinza hover:bg-paleta-azul2">PT-BR</button>
                 <button onClick={TextoEnUs} className="p-1 m-1 rounded-sm bg-paleta-cinza hover:bg-paleta-azul2">EN-US</button>
             </div>
             <p className="w-96 h-auto font-padrao p-1 border-4 border-paleta-cinza text-center rounded-lg lg:w-4/3">{textos[numTexto]}</p>
             <ul className="flex justify-center mt-5 mb-5 md:w-96 md:justify-around">
-                <li><a href="#"><img src="/assets/icons/linkedin.png" className="w-16 md:w-20 p-1" alt="Linkedin" /></a></li>
-                <li><a href="#"><img src="/assets/icons/github.png" className="w-16 md:w-20 p-1" alt="Github" /></a></li>
-                <li><a href="#"><img src="/assets/icons/instagram.png" className="w-16 md:w-20 p-1" alt="Instagram" /></a></li>
+                <li><a href="https://www.linkedin.com/in/guivargas95/" target="_blank"><img src="/assets/icons/linkedin.png" className="w-16 md:w-20 p-1" alt="Linkedin" /></a></li>
+                <li><a href="https://github.com/guivargas95" target="_blank"><img src="/assets/icons/github.png" className="w-16 md:w-20 p-1" alt="Github" /></a></li>
+                <li><a href="https://www.instagram.com/guii.vargas/" target="_blank"><img src="/assets/icons/instagram.png" className="w-16 md:w-20 p-1" alt="Instagram" /></a></li>
             </ul>
         </div>
     )
