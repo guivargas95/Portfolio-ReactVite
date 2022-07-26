d9e258ad81440313a0a372fdbb1b0a893cb33740
Guilherme Vargas <guisouzavargas@outlook.com>
2022-07-25 21:34:51 -0300

    corrigindo diretório de imagens

--- src/components/mainCarousel/index.tsx
+++ src/components/mainCarousel/index.tsx
@@ -2,7 +2,7 @@ export default function MainCarousel() {
 
     return (
         <div className="flex bg-paleta-azul4">
-            <img className="shadow-lg shadow-black w-full h-56 md:h-96" src="/assets/banners/pgr2.jpg" alt="" />
+            <img className="shadow-lg shadow-black w-full h-56 md:h-96" src="/assets/banners/pgr.jpg" alt="" />
         </div>
     )
 }
