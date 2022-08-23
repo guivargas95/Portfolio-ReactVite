dc278ec19cf3d48cdcfbc7239923433b274cc426
Guilherme Vargas <guisouzavargas@outlook.com>
2022-08-23 13:37:54 -0300

    Corrigindo css e Type do repository

--- src/components/githubRepositories/index.tsx
+++ src/components/githubRepositories/index.tsx
@@ -7,14 +7,15 @@ export default function GithubRepositories() {
 
     return (
         <section className="bg-paleta-azul4 font-padrao">
-            <div className="p-5">
+            <div className="p-5 grid justify-center">
                 <h2 className="p-5 text-center text-white text-4xl">Github Projects</h2>
-                <ul>
+                <ul className="w-80 md:w-96">
                     {repositories?.map(repo => {
                         return (
-                            <li className="text-paleta-cinza p-3" key={repo.name}>
+                            <li className="text-paleta-cinza p-3 border-4 border-paleta-cinza rounded-lg mt-4 grid" key={repo.name}>
                                 <h3 className="text-center text-lg font-bold">{repo.name}</h3>
                                 <p className="text-center">{repo.description}</p>
+                                <button className="p-1 mt-2 mb-2 bg-yellow-200"><a target="_blank" href={repo.html_url}>Vem ver!</a></button>
                             </li>
                         )
                     })}
